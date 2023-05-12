6ea68f250475b592ad7e76956d8c8af0efbfe2cf
Victor Guzmán Berrio <victorguzber@gmail.com>
2023-05-12 05:42:54 -0500

    open file

--- app/scripts/controllers/upload-photos.js
+++ app/scripts/controllers/upload-photos.js
@@ -10,18 +10,5 @@
 angular
   .module("testMotoSmartApp")
   .controller("UploadPhotosCtrl", function ($scope) {
-    $scope.init = () => {
-      document.addEventListener("DOMContentLoaded", function () {
-        let elems = document.querySelectorAll(".fixed-action-btn");
-        let instances = M.FloatingActionButton.init(elems, options);
-      });
-
-      // Or with jQuery
-
-      $(document).ready(function () {
-        $(".fixed-action-btn").floatingActionButton();
-      });
-    };
-
-    $scope.init();
+    $scope.openFloatingActionButton = () => {};
   });
