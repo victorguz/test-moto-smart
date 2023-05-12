5b924df409fe382144e5466d08383aa720c1698e
Victor Guzmán Berrio <victorguzber@gmail.com>
2023-05-12 01:25:01 -0500

    start project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name testMotoSmartApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the testMotoSmartApp
+ */
+angular.module('testMotoSmartApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name testMotoSmartApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the testMotoSmartApp
+ */
+angular.module('testMotoSmartApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
