606ea21e32c42c72f22de7b32bff1082dc8d4a84
Victor Guzmán Berrio <victorguzber@gmail.com>
2023-05-16 22:04:47 -0500

    -

--- app/scripts/controllers/last-form.js
+++ app/scripts/controllers/last-form.js
@@ -14,15 +14,27 @@ angular
     $scope.first_value = "";
     $scope.percent = "";
     $scope.isEnabledContinue = () => {
-      if ($scope.toggle && $scope.first_value && $scope.percent) {
-        return true;
-      }
-      if (!$scope.toggle && $scope.first_value) {
+      if (
+        ($scope.toggle && $scope.first_value && $scope.percent) ||
+        (!$scope.toggle && $scope.first_value)
+      ) {
         return true;
       }
       return false;
     };
     $scope.setPercent = (event) => {
       $scope.percent = event.target.value;
+    };
+    $scope.getPercentLength = () => {
+      return $scope.percent ? String($scope.percent).length : 0;
+    };
+    $scope.getPercentX100 = () => {
+      return $scope.percent * 100;
+    };
+    $scope.getFirstValueLength = () => {
+      return $scope.first_value ? String($scope.first_value).length : 0;
+    };
+    $scope.getValueMotoPuntos = () => {
+      return $scope.first_value - ($scope.first_value * $scope.percent) / 100;
     };
   });
